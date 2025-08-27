25ab6311a33854e246b105d3d313b187075fcf7a
MiqueiasMendez <92160953+MiqueiasMendez@users.noreply.github.com>
2025-08-27 13:56:58 -0300

    Add src/services/api/api.ts 1756313820426

---
+++ src/services/api/api.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Exportações de todos os serviços de API
+ */
+
+// Exporta a função de checagem de saúde da API
+export { checkApiHealth } from './api-utils';
+
+// Exporta os serviços
+export { ordersService, customersService, productsService } from './api-service';
